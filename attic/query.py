4085fc5db559350ad05f0be28429a99bb28fad7e
Ricardo E. Gonzalez <ricardog@ricardog.com>
2017-11-12 22:42:37 +0000

    Fixed bounds handling and cleanup
    - Added comment on how to insert GDB file into PostGIS DB (use ogr2ogr).
    
    - Fix how bounds get handled.  It is now possible to select a bounding
      box smaller than the reference file.
    
    - Write output to disk (always same name so it will overwitr existing
      file).

--- attic/query.py
+++ attic/query.py
@@ -8,12 +8,16 @@ import psycopg2
 import rasterio
 from rasterio.plot import show
 import sys
+import time
 
 import pdb
 
 #
 # To create the DB:
 # CREATE DATABASE groads WITH ENCODING='UTF8' LC_CTYPE='en_US.UTF-8' LC_COLLATE='en_US.UTF-8' OWNER=vagrant TEMPLATE=template0 CONNECTION LIMIT=-1;
+#
+# To insert roads data into database
+# ogr2ogr -f "PostgreSQL" PG:"host=hostname port=5432 dbname=groads user=username password=passwd" groads1.0/groads-v1-global-gdb/gROADS_v1.gdb Global_Roads
 #
 # To create the index
 # CREATE INDEX global_roads_gidx ON global_roads USING GIST (wkb_geometry);
@@ -48,40 +52,49 @@ def put(array, data):
   idx = np.ravel_multi_index((y, x), array.shape)
   np.put(array, idx, v)
   
-def do_query(bounds, affine):
+def do_query(bounds, affine, nodata, dtype):
   query_sql = read_query()
   params = bounds_to_params(bounds, affine)
   nrows = params['nrows']
   ncols = params['ncols']
   shape = (nrows, ncols)
+  ul = (bounds[0], bounds[3])  * ~affine
   
   params['nrows'] = min(int(1e6 / nrows), nrows)
-  params.update({'xoff': -180})
-  out = np.full(shape, -1.0, dtype=float)
+  params.update({'xoff': bounds[0]})
+  out = np.full(shape, nodata, dtype=dtype)
   
   conn = psycopg2.connect(dbname=DB_NAME, user=USER, host=HOST,
                           password=PASSWORD)
   cursor = conn.cursor()
 
-  for yoff in range(0, nrows, params['nrows']):
+  stime = time.time()
+  for yoff in range(int(ul[1]), int(ul[1]) + nrows, params['nrows']):
     print("rows: %d:%d" % (yoff, yoff + params['nrows']))
-    xb0, yb0 = (0, yoff) * affine
-    xb1, yb1 = (0, yoff + params['nrows']) * affine
-    print("bbox: %5.2f:%5.2f" % (yb0, yb1))
-    params['yoff'] = yb0
+    _, y0 = (0, yoff) * affine
+    _, y1 = (0, yoff + params['nrows']) * affine
+    print("bbox: %5.2f:%5.2f" % (y0, y1))
+    params['yoff'] = y0
     query_str = query_sql % params
-    print(query_str)
+    #print(query_str)
     cursor.execute(query_sql, params)
     while True:
       data = cursor.fetchmany(params['nrows'] * params['ncols'])
       if len(data) == 0:
         break
       put(out, data)
-  masked = ma.masked_equal(out, -1)
-  show(masked)
-  sys.exit()
+  masked = ma.masked_equal(out, nodata)
+  etime = time.time()
+  print("executed in %7.2fs" % (etime - stime))
+  return masked
 
 if __name__ == '__main__':
   #do_query((-180, -90, 180, 90), Affine(0.5, 0, -180, 0, -0.5, 90))
-  with rasterio.open('/Users/ricardog/src/eec/predicts/playground/ds/rcp/un_codes.tif') as ds:
-    do_query(ds.bounds, ds.meta['affine'])
+  ref = '/Users/ricardog/src/eec/predicts/playground/ds/rcp/un_codes.tif'
+  with rasterio.open(ref) as ds:
+    meta = ds.meta
+    meta.update({'dtype': 'float32', 'nodata': -1.0})
+    out = do_query(ds.bounds, ds.meta['affine'], -1.0, 'float32')
+    with rasterio.open('road-length.tif', 'w', **meta) as dst:
+      dst.write(out, 1)
+    show(out)

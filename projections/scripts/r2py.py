3e2d9c597ad658912ea85169c079649e2b11da3d
Ricardo E. Gonzalez <ricardog@ricardog.com>
2017-11-12 15:54:26 +0000

    Add support for roquefort generated .rds file
    Roquefort generates .rds files that wrap a model and associated
    data.frame ina list.  Compiling them has usually entailed me
    hand-extracting the model into a new file.
    
    This change unifies the processing used for the LUI models (which have
    two models in a list) with handling of roquefort-generated files.

--- projections/scripts/r2py.py
+++ projections/scripts/r2py.py
@@ -11,6 +11,19 @@ from ..r2py import glmermod
 from .. import predicts
 from .. import utils
 
+def doit(obj):
+  if 'lmerMod' in obj.rclass:
+    mod = lmermod.LMerMod(obj)
+  elif 'glmerMod' in obj.rclass:
+    mod = glmermod.GLMerMod(obj)
+  elif 'glm' in obj.rclass or 'lm' in obj.rclass:
+    mod = glm.GLM(obj)
+  else:
+    print('Skipping object of type %s' % str(obj.rclass))
+    return None
+  predicts.predictify(mod)
+  return mod
+
 def main():
   if len(sys.argv) != 2:
     print("Usage: %s <models.rds>" % os.path.basename(sys.argv[0]))
@@ -25,20 +38,13 @@ def main():
   print('%s:' % name)
   with open(os.path.join(outdir, base + '.py'), 'w') as ofile:
     obj = robjects.r('models <- readRDS("%s")' % fname)
-    if 'lmerMod' in obj.rclass:
-      mod = lmermod.LMerMod(obj)
-      predicts.predictify(mod)
-      ofile.write(mod.to_numba(pname))
-    elif 'glmerMod' in obj.rclass:
-      mod = glmermod.GLMerMod(obj)
-      predicts.predictify(mod)
-      ofile.write(mod.to_numba(pname))
-    elif 'list' in obj.rclass:
-        for mm in obj.items():
-          print("  %s" % mm[0])
-          mod = glm.GLM(mm[1])
-          predicts.predictify(mod)
+    if 'list' in obj.rclass:
+      for mm in obj.items():
+        print("  %s" % mm[0])
+        mod = doit(mm[1])
+        if mod:
           ofile.write(mod.to_numba(mm[0]))
     else:
-      print("Error: unknown R object class")
-      sys.exit(1)
+      mod = doit(obj)
+      if mod:
+        ofile.write(mod.to_numba(pname))

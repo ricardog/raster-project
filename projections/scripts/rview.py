1f6a6f23c0d6e4af66cc47266f02d8e5e3ad8070
Ricardo E. Gonzalez <ricardog@ricardog.com>
2017-11-13 18:27:10 +0000

    Add a colorbar to the on-screen display
    Helpful to understand what the colors mean.  Added options to override
    the max/min calculation because 4% of the cells can be a lot.  When
    displaying road length data I would see large swaths of red (which now
    indicates over the max).
    
    Change color palette to display empty cells in white.
    
    Added help strings for all options.

--- projections/scripts/rview.py
+++ projections/scripts/rview.py
@@ -6,6 +6,9 @@ import rasterio
 from rasterio.plot import show
 import gdal
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+import pdb
 
 import projections.tiff_utils as tu
 
@@ -18,17 +21,37 @@ def get_min_max(fname):
 
 @click.command()
 @click.argument('fname', type=click.Path(dir_okay=False))
-@click.option('-b', '--band', type=int, default=1)
-@click.option('-s', '--save', type=click.Path(dir_okay=False))
-@click.option('-t', '--title')
-def main(fname, band, title, save):
-  min, max = get_min_max(fname)
+@click.option('-b', '--band', type=int, default=1,
+              help='Which raster band to display (default: 1)')
+@click.option('-s', '--save', type=click.Path(dir_okay=False),
+              help='Save the resulting image to disk.')
+@click.option('-t', '--title',
+              help='Title of the image (default: file name)')
+@click.option('--max', type=float,
+              help='Upper bound for color display.  Any cells less than ' +
+              'this value will be shown in red.  By default it will ' +
+              'compute the maximum such that 98% of the cells are below ' +
+              'this value.')
+@click.option('--min', type=float,
+              help='Lower bound for color display.  Any cells less than ' +
+              'this value will be shown in black.  By default it will' +
+              'compute the minimum such that 2% of the cells are above ' +
+              'this value.')
+@click.option('--colorbar/--no-colorbar', default=True,
+              help='Display/hide a colorbar with the value range.')
+def main(fname, band, title, save, max, min, colorbar):
+  rmin, rmax = get_min_max(fname)
+  if max is None:
+    max = rmax
+  if min is None:
+    min = rmin
   if title is None:
     title = fname
   palette = copy(plt.cm.viridis)
-  #palette.set_over('g', 1.0)
-  #palette.set_under('r', 1.0)
-  palette.set_bad('#0e0e2c', 1.0)
+  palette.set_over('r', 1.0)
+  palette.set_under('k', 1.0)
+  #palette.set_bad('#0e0e2c', 1.0)
+  palette.set_bad('w', 1.0)
 
   src = rasterio.open(fname)
   #data = src.read(band, masked=True)
@@ -36,13 +59,20 @@ def main(fname, band, title, save):
     fig = plt.figure()
     #plt.gca().set_title(title)
     ax = plt.gca()
-    show((src, band), cmap=palette, ax=ax)
+    show((src, band), ax=ax, cmap=palette, title=title, vmin=min, vmax=max)
     fig.tight_layout()
     ax.axis('off')
     fig.savefig(save, transparent=True)
     plt.show()
   else:
-    ax = show((src, band), cmap=palette, title=title, vmin=min, vmax=max)
-
+    fig = plt.figure()
+    ax = plt.gca()
+    show((src, band), ax=ax, cmap=palette, title=title, vmin=min, vmax=max)
+    if colorbar:
+      divider = make_axes_locatable(ax)
+      cax = divider.append_axes("bottom", size="5%", pad=0.25)
+      plt.colorbar(ax.images[0], cax=cax, orientation='horizontal')
+    plt.show()
+    
 if __name__ == '__main__':
   main()

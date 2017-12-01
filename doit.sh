bcc906d1c5b2acfd5ac939bbf330d780486a6eba
Ricardo E. Gonzalez <ricardog@ricardog.com>
2017-12-01 13:08:59 +0000

    Convert to use parallel xargs
    Rather then trying to implement parallelism via & and wait, use the
    parallel option of xargs.  This requires the loop be silent as all the
    output gets sent to xargs.
    
    I poked around a bit to try to figure out how to use a third file
    descriptor but didn't figure it out right away and decided this was good
    enough.

--- doit.sh
+++ doit.sh
@@ -16,15 +16,11 @@ for dname in /data/luh2_v2/LUH2_v2f_SSP[0-9]_*; do
 	printf "  %-6s :: %s:%s\n" ${what} ${year0} ${year1}
 	if ${for_real}; then
 	    echo > /dev/null
-	    ./ipbes-project.py -p ${num_jobs} \
-			       -m /vagrant/models/sam/2017-11-22/ \
-			       ${what} ${scenario} ${year0}:${year1} > /dev/null
 	fi
     done
 
     ## Combine base layers into BII layer.
     for what in sr ab; do
-	printf "  %2s-bii :: %s:%s\n" ${what} ${year0} ${year1}
 	if [ "${what}" == "ab" ]; then
 	    v1="Abundance"
 	    v2="Ab"
@@ -34,13 +30,10 @@ for dname in /data/luh2_v2/LUH2_v2f_SSP[0-9]_*; do
 	fi
 	for (( year=$year0; year<$year1; year++ )); do
 	    ## Run num_jobs in parallel.
-	    ((i = i % num_jobs)); ((i++ == 0)) && wait
-	    rio calc --co "COMPRESS=lzw" --co "PREDICTOR=2" --masked \
-		-t float32 \
-		-o /out/luh2/${scenario}-BII${v2}-${year}.tif \
-		"(* (read 1 1) (read 2 1))" \
-		/out/luh2/${scenario}-${v1}-${year}.tif \
-		/out/luh2/${scenario}-CompSim${v2}-${year}.tif &
+	    echo "-o /out/luh2/${scenario}-BII${v2}-${year}.tif /out/luh2/${scenario}-${v1}-${year}.tif /out/luh2/${scenario}-CompSim${v2}-${year}.tif"
 	done
-    done
+    done | xargs -P ${num_jobs} -n 1 -l1 rio calc \
+		 --co "COMPRESS=lzw" --co "PREDICTOR=2" --masked \
+		 -t float32 \
+		 "(* (read 1 1) (read 2 1))"
 done

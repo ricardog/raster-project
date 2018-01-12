3ff07bbc85a548b2f7190feb43bd43f86a43c322
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-01-12 16:29:16 +0000

    Cleanup visualization script

--- ipbes-visualize.py
+++ ipbes-visualize.py
@@ -7,61 +7,65 @@ from bokeh.layouts import gridplot
 from bokeh.models import Range1d, ColumnDataSource, HoverTool, CrosshairTool
 from bokeh.palettes import Category20
 from bokeh.plotting import figure
-import numpy as np
 import pandas as pd
 import requests
 
-import projections.utils as utils
+import pdb
 
-output_file('ipbes.html')
+def csv2df(url, stype, syear, eyear):
+    s = requests.get(url % (stype, int(syear), int(eyear))).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    subset = df.loc[:, syear:eyear].T.reset_index()
+    subset.columns = ['Year'] + df['Name'].values.tolist()
+    return subset
 
-scenarios = utils.luh2_scenarios()
+def pline(p, df, column, color='black', line_width=6):
+    src = ColumnDataSource(data={
+        'year': df.Year,
+        'data': df[column],
+        'name': [column for n in range(len(df))]
+    })
+    p.line('year', 'data', source=src, line_width=line_width,
+           color=color)
+    
+scenarios = ('historical',
+             'ssp1_rcp2.6_image',
+             'ssp3_rcp7.0_aim',
+             'ssp4_rcp3.4_gcam',
+             'ssp4_rcp6.0_gcam',
+             'ssp5_rcp8.5_remind-magpie')
 plots = []
-for scenario in utils.luh2_scenarios():
+
+for scenario in scenarios:
     print(scenario)
     row = []
     for indicator in ('BIIAb', 'BIISR'):
         title = 'historical'
-        base_url = "http://ipbes.s3.amazonaws.com/summary/%s-%s-%s- 900-2014.csv"
         syear = '900'
         eyear = '2014'
+        base_url = "http://ipbes.s3.amazonaws.com/summary/" + \
+                   "%s-%s-%%s-%%4d-%%4d.csv" % (scenario, indicator)
         if scenario != 'historical':
             ssp, rcp, model = scenario.upper().split('_')
             title = '%s -- %s / %s' % (indicator, ssp, rcp)
-            base_url = "http://ipbes.s3.amazonaws.com/summary/%s-%s-%s-2015-2100.csv"
             syear = '2015'
             eyear = '2100'
         p = figure(title=title)
         p.y_range = Range1d(0.45, 1)
-        s = requests.get(base_url % (scenario, indicator, 'subreg')).content
-        df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-        #df = pd.read_csv('ipbes-upload/%s-%s-subreg-2015-2100.csv' % (scenario, indicator))
-        subset = df.loc[:, syear:eyear].T
-        subset.columns = df['Name']
-        subset.reset_index(inplace=True)
-        subset = subset.rename(columns={'index': 'Year'})
+        subset = csv2df(base_url, 'subreg', syear, eyear)
         mypalette=Category20[len(subset.columns)]
 
         for idx, col in enumerate(subset.columns):
             if col in ('Year', 'Excluded'):
                 continue
-            src = ColumnDataSource(data={
-                'year': subset.Year,
-                'data': subset[col],
-                'name': [col for n in range(len(subset))]
-            })
-            p.line('year', 'data', source=src, line_width=4, color=mypalette[idx])
-            
-        s = requests.get(base_url % (scenario, indicator, 'global')).content
-        df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-        glob = df.loc[:, syear:eyear].T.reset_index()
-        src = ColumnDataSource(data={
-            'year': glob['index'],
-            'data': glob[0],
-            'name': ['Global' for n in range(len(glob))]
-        })
-        p.line('year', 'data', source=src, line_width=8, color='black')
-        p.add_tools(HoverTool(tooltips=[('Year', '@year'), (indicator, '@data'), ('Region', '@name')]))
+            pline(p, subset, col, mypalette[idx], 4)
+
+        glob = csv2df(base_url, 'global', syear, eyear)
+        pline(p, glob, 'Global', 'black')
+
+        p.add_tools(HoverTool(tooltips=[('Year', '@year'),
+                                        (indicator, '@data'),
+                                        ('Region', '@name')]))
         row.append(p)
     plots.append(row)
 grid = gridplot(plots, sizing_mode='scale_width')

155bd849ef8c21df28a243e7539358e03dc329cc
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-01-12 14:14:09 +0000

    Scale (weight) abundance & richness when computing BII
    Abundance gets caled with NPP and richness with vertebrate richness.

--- do-hist-one.sh
+++ do-hist-one.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -e
 year=$1
 scenario='historical'
-DATA_ROOT=${DATA_ROOT:=/Users/ricardog/src/eec/data}
-OUTDIR=${OUTDIR:=/Users/ricardog/src/eec/predicts/playground/ds}
+export DATA_ROOT=${DATA_ROOT:=/Users/ricardog/src/eec/data}
+export OUTDIR=${OUTDIR:=/Users/ricardog/src/eec/predicts/playground/ds}
 
 ## Generate the four base layers.
 for what in sr cs-sr ab cs-ab; do
@@ -18,15 +18,20 @@ for what in sr ab; do
     if [ "${what}" == "ab" ]; then
 	v1="Abundance"
 	v2="Ab"
+	v3="npp"
     else
 	v1="Richness"
 	v2="SR"
+	v3="vertebrate-richness"
     fi
     rio clip ${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif \
 	--like ${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
 	--output ${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif
-	
+
     rio calc --co "COMPRESS=lzw" --co "PREDICTOR=2" --masked \
-	-t float32 "(* (read 1 1) (read 2 1))" \
-	-o ${OUTDIR}/luh2/${scenario}-BII${v2}-${year}.tif ${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif ${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif
+	-t float32 "(* (read 1 1) (read 2 1) (read 3 1))" \
+	-o ${OUTDIR}/luh2/${scenario}-BII${v2}-${year}.tif \
+	${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
+	${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif \
+	${OUTDIR}/luh2/${v3}.tif
 done

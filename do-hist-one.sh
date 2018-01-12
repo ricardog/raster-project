281031b048a98a61ed2f8b5dc45c16179eafc98f
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-01-12 15:24:16 +0000

    Add npp/vsr weight when computing BII

--- do-hist-one.sh
+++ do-hist-one.sh
@@ -24,14 +24,26 @@ for what in sr ab; do
 	v2="SR"
 	v3="vertebrate-richness"
     fi
+
+    tempweight=$(basename $0)
+    TMPFILE=$(mktemp -t ${tempweight})
+    if [ $? -ne 0 ]; then
+	echo "$0: Can't create temp file, exiting..."
+        exit 1
+    fi
+
     rio clip ${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif \
 	--like ${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
 	--output ${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif
 
+    rio clip ${OUTDIR}/luh2/${v3}.tif \
+	--like ${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
+	--output ${TMPFILE}
+    
     rio calc --co "COMPRESS=lzw" --co "PREDICTOR=2" --masked \
 	-t float32 "(* (read 1 1) (read 2 1) (read 3 1))" \
 	-o ${OUTDIR}/luh2/${scenario}-BII${v2}-${year}.tif \
 	${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
 	${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif \
-	${OUTDIR}/luh2/${v3}.tif
+	${TMPFILE}
 done

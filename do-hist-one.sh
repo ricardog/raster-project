075c7dc99e587b443168aad8a59ceb5218ac8d87
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-01-12 15:41:43 +0000

    Undo most of my last change
    Weights come into play only when computing a regional (or global)
    summary.

--- do-hist-one.sh
+++ do-hist-one.sh
@@ -18,32 +18,18 @@ for what in sr ab; do
     if [ "${what}" == "ab" ]; then
 	v1="Abundance"
 	v2="Ab"
-	v3="npp"
     else
 	v1="Richness"
 	v2="SR"
-	v3="vertebrate-richness"
-    fi
-
-    tempweight=$(basename $0)
-    TMPFILE=$(mktemp -t ${tempweight})
-    if [ $? -ne 0 ]; then
-	echo "$0: Can't create temp file, exiting..."
-        exit 1
     fi
 
     rio clip ${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif \
 	--like ${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
 	--output ${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif
 
-    rio clip ${OUTDIR}/luh2/${v3}.tif \
-	--like ${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
-	--output ${TMPFILE}
-    
     rio calc --co "COMPRESS=lzw" --co "PREDICTOR=2" --masked \
-	-t float32 "(* (read 1 1) (read 2 1) (read 3 1))" \
+	-t float32 "(* (read 1 1) (read 2 1))" \
 	-o ${OUTDIR}/luh2/${scenario}-BII${v2}-${year}.tif \
 	${OUTDIR}/luh2/${scenario}-${v1}-${year}.tif \
-	${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif \
-	${TMPFILE}
+	${OUTDIR}/luh2/${scenario}-CompSim${v2}-${year}.tif
 done

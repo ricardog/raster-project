d9f8bc454a736224333f38528e610a0d9d3be53f
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-01-19 16:39:54 +0000

    Add support for plotting projected and source data
    This is a big change that adds support for plotting projected as well as
    source time-series population data.  There are currently a number of
    click arguments that will plot HYDE, SPS, or projected data.  Each reads
    a different data source and plots the worldwide human population total.

--- attic/hpd-check.py
+++ attic/hpd-check.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
-import rasterio
-from rasterio.plot import show
+from itertools import groupby
+import os
+import re
+import sys
+
+from bokeh.io import output_file, show, save
+from bokeh.layouts import gridplot
+from bokeh.models import Range1d, ColumnDataSource, HoverTool, CrosshairTool
+from bokeh.palettes import Category20, brewer, viridis
+from bokeh.plotting import figure
+
+import click
+
+import matplotlib.pyplot as plt
 import numpy as np
 import numpy.ma as ma
-import sys
+import pandas as pd
+import rasterio
+from rasterio.plot import show
 
 import pdb
 
@@ -23,34 +37,166 @@ def rcs(height, res, left, bottom, right, top):
          (res[0] * np.pi/180) * earth_radius() ** 2 / 1e6)
   return vec.reshape((vec.shape[0], 1))
 
+def _one(fname, scale=True, band=1):
+  ds = rasterio.open(fname)
+  area = rcs(ds.height, ds.res, *ds.bounds)
+  data = ds.read(band, masked=True).filled(0)
+  data[np.where(np.isnan(data))] = 0
+  adj = data * area if scale else data
+  return adj.sum()
+
 def one(name, fname, scale=True, band=1):
   ds = rasterio.open(fname)
   area = rcs(ds.height, ds.res, *ds.bounds)
   data = ds.read(band, masked=True).filled(0)
   data[np.where(np.isnan(data))] = 0
   adj = data * area if scale else data
-  print("%-10s: %e" % (name, adj.sum()))
-  print("%10s: %e" % ('max', data.max()))
+  print("%-30s: %e" % (name, adj.sum()))
+  #print("%10s: %e" % ('max', data.max()))
   if not scale:
-    print("%10s: %e" % ('max', (data / area).max()))
+    print("%30s: %e" % ('max', (data / area).max()))
 
-for year in (2010, 2099):
-  scenario = 'ssp3'
-  one('%s/%d' % (scenario, year),
-      'netcdf:%s/luh2/sps.nc:%s' % (utils.outdir(), scenario),
-      False, year - 2009)
+def get_files(dname, what):
+  files = groupby(map(lambda f: f.rsplit('-', 2),
+                      map(lambda p: os.path.splitext(p)[0],
+                          filter(lambda f: re.search(r'-%s-\d+.tif$' % what, f),
+                                 sorted(os.listdir(dname))))),
+                  key=lambda s: s[0])
+  out = {}
+  for scenario, content in files:
+    out[scenario] = ['%s-%s-%s.tif' % (s, w, y)
+                     for s, w, y in tuple(content)]
+  return out
 
-for name, fname, scale in (('gluds qd', '/out/luh2/gluds00ag.tif', True),
-                           ('v4 qd', '/out/luh2/grumps4.tif', True),
-#                           ('1950', '/Volumes/Vagrant 155/playground/ds/luh2/historical-hpd-1950.tif', True),
-                           ('sps3/2015', 'netcdf:/out/luh2/sps.nc:ssp3', False),
-                           ('v4', utils.grumps4(), True),
-                           ('gluds', utils.grumps1(), True),):
-  one(name, fname, scale)
+def pline(p, df, column, legend=None, color='black', line_width=3):
+    src = ColumnDataSource(data={
+        'year': df.index,
+        'data': df[column],
+        'name': [legend for n in range(len(df))]
+    })
+    if legend is None:
+      legend = column
+    p.line('year', 'data', source=src, line_width=line_width,
+           legend=legend, color=color)
 
-#                           ('sps1', '/data/sps/SSP1_NetCDF/total/NetCDF/ssp1_2010.nc', False),
-#                           ('sps2', '/data/sps/SSP2_NetCDF/total/NetCDF/ssp2_2010.nc', False),
-#                           ('sps3', '/data/sps/SSP3_NetCDF/total/NetCDF/ssp3_2010.nc', False),
-#                           ('sps4', '/data/sps/SSP4_NetCDF/total/NetCDF/ssp4_2010.nc', False),
-#                           ('sps5', '/data/sps/SSP5_NetCDF/total/NetCDF/ssp5_2010.nc', False),
+def bokeh_plot(dfs):
+  p = figure(title='Worldwide Human Population')
+  mypalette=Category20[len(dfs)]
+  for idx, scenario in enumerate(dfs):
+    df = dfs[scenario]
+    name = scenario.split('_', 1)[0]
+    pline(p, df, scenario, name, mypalette[idx], 3)
+  p.add_tools(HoverTool(tooltips=[('Year', '@year'),
+                                  ('Population', '@data'),
+                                  ('Scenario', '@name')]))
+  p.legend.location = "top_left"
+  return p
+
+def do_plot(dfs, bokeh, out):
+  if bokeh:
+    p = bokeh_plot(dfs)
+    if out:
+      output_file(out)
+    save(p)
+  else:
+    ax = None
+    for key in dfs:
+      ax = dfs[key].plot(ax=ax)
+    ax.set_title('Worldwide Human Population')
+    plt.show()
   
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+  if ctx.invoked_subcommand is None:
+    click.echo('I was invoked without subcommand')
+    projections()
+
+@cli.command()
+@click.option('--datadir', type=click.Path(dir_okay=True, file_okay=False),
+              default='/data')
+@click.option('--outdir', type=click.Path(dir_okay=True, file_okay=False),
+              default='/out/luh2')
+@click.option('-o', '--out', type=click.Path(dir_okay=False, file_okay=True))
+@click.option('-r', '--raw', is_flag=True, type=str, default=False)
+@click.option('-b', '--bokeh', is_flag=True, default=False)
+def sps(datadir, outdir, out, raw, bokeh):
+  dfs = {}
+  for scenario in map(lambda n: 'ssp%d' % n, range(1, 6)):
+    print(scenario)
+    df = pd.DataFrame()
+    if raw:
+      raise NotImplementedError('SPS unscaled support not implemented')
+    else:
+      ds = rasterio.open('netcdf:' +
+                         os.path.join(outdir, 'sps.nc:%s' % scenario))
+      years = tuple(map(lambda idx: int(ds.tags(idx)['NETCDF_DIM_time']),
+                        ds.indexes))
+      df[scenario] = tuple(map(lambda y: ds.read(years.index(y) + 1,
+                                                 masked=True).sum(), years))
+      df.index = years
+    dfs[scenario] = df
+  do_plot(dfs, bokeh, out)
+  
+@cli.command()
+@click.option('--outdir', type=click.Path(dir_okay=True, file_okay=False),
+              default='/out/luh2')
+@click.option('-o', '--out', type=click.Path(dir_okay=False, file_okay=True))
+@click.option('-b', '--bokeh', is_flag=True, default=False)
+def hyde(outdir, out, bokeh):
+  dfs = {}
+  df = pd.DataFrame()
+  ds = rasterio.open('netcdf:' + os.path.join(outdir, 'hyde.nc:popd'))
+  years = tuple(map(lambda idx: int(ds.tags(idx)['NETCDF_DIM_time']),
+                    ds.indexes))
+  df['historical'] = tuple(map(lambda y: _one(ds.name, True,
+                                              years.index(y) + 1),
+                               years))
+  df.index = years
+  dfs['historical'] = df
+  do_plot(dfs, bokeh, out)
+
+
+@cli.command()
+@click.option('--outdir', type=click.Path(dir_okay=True, file_okay=False),
+              default='/out/luh2')
+@click.option('-o', '--out', type=click.Path(dir_okay=False, file_okay=True))
+@click.option('--what', type=str, default='hpd')
+@click.option('-b', '--bokeh', is_flag=True, default=False)
+def projections(outdir, out, what, bokeh):
+  files = get_files(outdir, what)
+  dfs = {}
+  for scenario in files:
+    print(scenario)
+    df = pd.DataFrame()
+    df[scenario] = tuple(map(lambda f: _one(os.path.join(outdir, f), True, 1),
+                             files[scenario]))
+    df.index = tuple(map(lambda f: int(os.path.splitext(f)[0].rsplit('-', 1)[-1]),
+                         files[scenario]))
+    dfs[scenario] = df
+  do_plot(dfs, bokeh, out)
+
+
+def old_school():
+  for year in (2010, 2099):
+    scenario = 'ssp3'
+    one('%s/%d' % (scenario, year),
+        'netcdf:%s/luh2/sps.nc:%s' % (utils.outdir(), scenario),
+        False, year - 2009)
+
+  for name, fname, scale in (('gluds qd', '/out/luh2/gluds00ag.tif', True),
+                             ('v4 qd', '/out/luh2/grumps4.tif', True),
+  #                           ('1950', '/Volumes/Vagrant 155/playground/ds/luh2/historical-hpd-1950.tif', True),
+                             ('sps3/2015', 'netcdf:/out/luh2/sps.nc:ssp3', False),
+                             ('v4', utils.grumps4(), True),
+                             ('gluds', utils.grumps1(), True),):
+    one(name, fname, scale)
+
+  #                           ('sps1', '/data/sps/SSP1_NetCDF/total/NetCDF/ssp1_2010.nc', False),
+  #                           ('sps2', '/data/sps/SSP2_NetCDF/total/NetCDF/ssp2_2010.nc', False),
+  #                           ('sps3', '/data/sps/SSP3_NetCDF/total/NetCDF/ssp3_2010.nc', False),
+  #                           ('sps4', '/data/sps/SSP4_NetCDF/total/NetCDF/ssp4_2010.nc', False),
+  #                           ('sps5', '/data/sps/SSP5_NetCDF/total/NetCDF/ssp5_2010.nc', False),
+  
+if __name__ == '__main__':
+  cli()

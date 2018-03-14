ec014aedb0b547be0d4cb5ade93909e593e1e54a
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-03-14 14:46:37 +0000

    Script to clip pixel values to (min, max)
    Where min and max are taken as optios on the CLI.

---
+++ clip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import click
+import numpy as np
+import rasterio
+
+import pdb
+
+@click.command()
+@click.argument('infile', type=click.Path(dir_okay=False))
+@click.argument('outfile', type=click.Path(dir_okay=False))
+@click.option('--min', 'a_max', type=float, default=None,
+              help='Min value to clip to (default: unbounded)')
+@click.option('--max', 'a_min', type=float, default=None,
+              help='Max value to clip to (default: unbounded)')
+def clip(infile, outfile, a_min=None, a_max=None):
+    with rasterio.open(infile) as src:
+      with rasterio.open(outfile, 'w', **src.meta) as dst:
+        for block_index, window in src.block_windows(1):
+          data = src.read(masked=True, window=window)
+          clipped = np.clip(data, a_min, a_max)
+          dst.write(clipped, window=window)
+
+if __name__ == '__main__':
+  clip()
+  

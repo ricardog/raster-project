477b308178c8a47de404357eebfb27ddd4c1c4cd
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-03-14 17:53:43 +0000

    Move core of clip routine into a library
    So it can be called from a projection script.

--- clip.py
+++ clip.py
@@ -5,7 +5,8 @@ import numpy as np
 import numpy.ma as ma
 import rasterio
 
-import pdb
+import projections.raster_utils as ru
+
 
 @click.command()
 @click.argument('infile', type=click.Path(dir_okay=False))
@@ -17,23 +18,7 @@ import pdb
 @click.option('--mask', is_flag=True, default=False,
               help='Mask (insted of clip) pixels outside the bounds')
 def clip(infile, outfile, a_min=None, a_max=None, mask=False):
-  if a_min is None and a_max is None:
-    print("Please specify min, max, or both")
-    return
-  
-  with rasterio.open(infile) as src:
-    with rasterio.open(outfile, 'w', **src.meta) as dst:
-      for block_index, window in src.block_windows(1):
-        data = src.read(masked=True, window=window)
-        if mask:
-          clipped = data
-          if a_max:
-            clipped = ma.masked_greater(clipped, a_max)
-          if a_min:
-            clipped = ma.masked_less(clipped, a_min)
-        else:
-          clipped = np.clip(data, a_min, a_max)
-        dst.write(clipped.filled(src.nodata), window=window)
+  ru.clip(infile, outfile, a_min, a_max, mask)
 
 if __name__ == '__main__':
   clip()

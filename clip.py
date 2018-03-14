494f7192f86e165762c564772b76110fe680bb75
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-03-14 15:10:46 +0000

    Add option to mask value outside the valid range
    Any cells with values outside the range get turned into nodata values.

--- clip.py
+++ clip.py
@@ -2,6 +2,7 @@
 
 import click
 import numpy as np
+import numpy.ma as ma
 import rasterio
 
 import pdb
@@ -13,7 +14,9 @@ import pdb
               help='Min value to clip to (default: unbounded)')
 @click.option('--max', 'a_max', type=float, default=None,
               help='Max value to clip to (default: unbounded)')
-def clip(infile, outfile, a_min=None, a_max=None):
+@click.option('--mask', is_flag=True, default=False,
+              help='Mask (insted of clip) pixels outside the bounds')
+def clip(infile, outfile, a_min=None, a_max=None, mask=False):
   if a_min is None and a_max is None:
     print("Please specify min, max, or both")
     return
@@ -22,8 +25,15 @@ def clip(infile, outfile, a_min=None, a_max=None):
     with rasterio.open(outfile, 'w', **src.meta) as dst:
       for block_index, window in src.block_windows(1):
         data = src.read(masked=True, window=window)
-        clipped = np.clip(data, a_min, a_max)
-        dst.write(clipped, window=window)
+        if mask:
+          clipped = data
+          if a_max:
+            clipped = ma.masked_greater(clipped, a_max)
+          if a_min:
+            clipped = ma.masked_less(clipped, a_min)
+        else:
+          clipped = np.clip(data, a_min, a_max)
+        dst.write(clipped.filled(src.nodata), window=window)
 
 if __name__ == '__main__':
   clip()

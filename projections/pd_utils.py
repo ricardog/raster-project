a3353e1b4c3e181958a877c8e2485c2c7eed4201
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-08-24 16:34:34 +0000

    Update pd_utils for python3
    Sadly, still not working :(.

--- projections/pd_utils.py
+++ projections/pd_utils.py
@@ -12,17 +12,18 @@ def save_pandas(fname, data):
         filename to use
     data: Pandas DataFrame or Series
     '''
-    np.save(open(fname, 'w'), data)
+    np.save(open(fname, 'wb'), data)
     if len(data.shape) == 2:
-        meta = data.index,data.columns
+        meta = data.index, data.columns
     elif len(data.shape) == 1:
         meta = (data.index,)
     else:
         raise ValueError('save_pandas: Cannot save this type')
     s = pickle.dumps(meta)
-    s = s.encode('string_escape')
-    with open(fname, 'a') as f:
-        f.seek(0, 2)
+    import pdb; pdb.set_trace()
+    #s = s.encode('string_escape')
+    with open(fname, 'ab') as f:
+        f.seek(0, os.SEEK_END)
         f.write(s)
 
 def load_pandas(fname, mmap_mode='r'):
@@ -36,11 +37,14 @@ def load_pandas(fname, mmap_mode='r'):
         Same as numpy.load option
     '''
     values = np.load(fname, mmap_mode=mmap_mode)
-    with open(fname) as f:
+    with open(fname, 'rb') as f:
         numpy.lib.format.read_magic(f)
         numpy.lib.format.read_array_header_1_0(f)
-        f.seek(values.dtype.alignment*values.size, 1)
-        meta = pickle.loads(f.readline().decode('string_escape'))
+        f.seek(values.dtype.alignment * values.size, 1)
+        data = f.readline()
+        #meta = pickle.loads(data.decode('string_escape'))
+        import pdb; pdb.set_trace()
+        meta = pickle.loads(data)
     if len(meta) == 2:
         return pd.DataFrame(values, index=meta[0], columns=meta[1])
     elif len(meta) == 1:

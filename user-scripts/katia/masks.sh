aad56c9ea62167863b3a0cca17b050c886fe7697
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-12-17 11:12:44 +0000

    Add support for selection of all-touched or not

--- user-scripts/katia/masks.sh
+++ user-scripts/katia/masks.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 set -e
+at=-at
 dir="/Volumes/Macintosh HD/Users/ricardog/tmp/katia"
 islands=KS_layers/Islands_fromIgor_edited.shp
 mainland=KS_layers/Mainland_fromIgor_edited.shp
 
 
-gdal_rasterize -tap -init 0 -burn 1 -a FID_1 \
+gdal_rasterize -tap -init 0 -burn 1 -a FID_1 ${at} \
 	       -ot Byte -tr 0.008333333333333 0.008333333333333 \
 	       -co "COMPRESS=lzw" -co "PREDICTOR=2" \
-	       "${dir}/${islands}" islands_fromIgor_edited.tif
+	       "${dir}/${islands}" islands-from-igor-edited${at}.tif
 
 
-gdal_rasterize -tap -init 0 -burn 1 -a FID_1 \
+gdal_rasterize -tap -init 0 -burn 1 -a FID_1 ${at} \
 	       -ot Byte -tr 0.008333333333333 0.008333333333333 \
 	       -co "COMPRESS=lzw" -co "PREDICTOR=2" \
-	       "${dir}/${mainland}" mainland_fromIgor_edited.tif
+	       "${dir}/${mainland}" mainland-from-igor-edited${at}.tif
 

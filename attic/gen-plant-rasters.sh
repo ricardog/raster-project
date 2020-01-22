5371d24bdd9405e625e73e3cbf50f79fff39441a
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-01-22 09:32:36 +0000

    Add "support" for generating 0.25° rasters
    By support I mean commented out code that can work for either 1km or
    0.25°.

--- attic/gen-plant-rasters.sh
+++ attic/gen-plant-rasters.sh
@@ -5,17 +5,21 @@ set -e
 #function join_by { local IFS="$1"; shift; echo "$*"; }
 function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
 
-COMMONS=(Banana Cacao Coffee Eucalyptus Pine)
-SIMPLE=("Oil Palm " "Oil Palm Mix" "Fruit Mix" "Unknown"
+COMMONS=(Banana Cacao Coffee Eucalyptus Pine Rubber)
+SIMPLE=("Oil Palm " "Oil Palm Mix" "Fruit Mix" "Rubber Mix" "Unknown"
 	"Wood fiber / timber")
 
-REFERENCE=/Users/ricardog/src/eec/predicts/playground/ds/1km/un_codes-full.tif
-ICEWTR='zip:///Users/ricardog/src/eec/data/1km/ICE.zip!/ICE_1km_2005.bil'
+#REFERENCE=/Users/ricardog/src/eec/predicts/playground/ds/1km/un_codes-full.tif
+#ICEWTR='zip:///Users/ricardog/src/eec/data/1km/ICE.zip!/ICE_1km_2005.bil'
+#outdir=sdpt_1km
+
+REFERENCE=/Users/ricardog/src/eec/predicts/playground/ds/luh2/un_codes-full.tif
+ICEWTR='netcdf:/Users/ricardog/src/eec/data/luh2_v2/staticData_quarterdeg.nc:icwtr'
+outdir=sdpt-qd
 
 JOINED=$(join_by "', '" "${COMMONS[@]}")
 NOT_IN=$(printf "common_name NOT IN ('%s')" "${JOINED}")
-
-outdir=plant-db-rasters
+outdir=sdpt_qd
 
 for common in "${COMMONS[@]}"; do
     echo "${common}"
@@ -38,7 +42,7 @@ for name in "${SIMPLE[@]}"; do
     fi
     where_clause=$(printf "WHERE species_simp='%s' AND %s" "${name}" "${NOT_IN}")
     caffeinate ./plant_db.py -n 4 trees "${where_clause}" ${REFERENCE} \
-	       plant-db-rasters/${fname}.tif
+	       ${outdir}/${fname}.tif
     ./partial-to-full.py ${outdir}/${fname}-full.tif \
 			 ${outdir}/${fname}.tif "${ICEWTR}"
 done

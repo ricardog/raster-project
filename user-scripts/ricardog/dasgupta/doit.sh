2529be9278e5aa03ea2ad0c6da655ec846ba72e8
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-06-09 15:34:31 +0100

    Add parallelism to doit.sh
    Since the matrix of models, and scenarios keeps growing, I decided to
    add support for running the scenarios (late, early, etc.) in parallel.
    
    Adding a new script (run.sh) which runs all scenarios for one model.

--- user-scripts/ricardog/dasgupta/doit.sh
+++ user-scripts/ricardog/dasgupta/doit.sh
@@ -1,29 +1,71 @@
 #!/bin/bash -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+die() {
+    printf '\033[1;31mERROR:\033[0m %s\n' "$@" >&2  # bold red
+    exit 1
+}
+
+einfo() {
+    printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2  # bold cyan
+}
+
+usage() {
+      echo "Usage: $0 [ -j JOBS ] [ -y YEARS ] [-m MODELS]" 1>&2 
+}
+
+JOBS=4
+MODELS="base ageclass nohpd nohpd-ageclass"
 YEARS=2020:2061:5
-for model in base ageclass nohpd nohpd-ageclass; do
-    echo ${model}
-    model_dir=$HOME/src/eec/predicts/models/dasgupta/2020-05-26/${model}
+ODIR=${OUTDIR:-/out}
+
+while getopts "p:m:y:h" options; do
+    case "${options}" in
+	p)
+	    JOBS=${OPTARG}
+	    re_isanum='^[0-9]+$'
+	    if ! [[ $JOBS =~ $re_isanum ]] ; then
+		die "JOBS must be a positive, whole number."
+	    elif [ $JOBS -eq "0" ]; then
+		die "JOBS must be greater than zero."
+	    fi
+	    ;;
+	y)
+	    YEARS=${OPTARG}
+	    ;;
+	m)
+	    MODELS=${OPTARG}
+	    ;;
+	h)
+	    usage
+	    exit 1
+	    ;;
+	:)
+	    die "-${OPTARG} requires an argument."
+	    ;;
+	*)
+	    die "Unexpected argument"
+	    ;;
+    esac
+done
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for model in ${MODELS}; do
+    einfo ${model}
+    if [[ -d /out ]]; then
+	model_dir=/out/models/dasgupta/2020-05-26/${model}
+    else
+	model_dir=$HOME/src/eec/predicts/models/dasgupta/2020-05-26/${model}
+    fi
     for scene in base early late_23 late_26 late_29 ; do
-	echo ${scene}
-	${DIR}/dasgupta.py project -m ${model_dir} -f ab ${scene} ${YEARS}
-	${DIR}/dasgupta.py project -m ${model_dir} ab ${scene} ${YEARS}
-
-	${DIR}/dasgupta.py project -m ${model_dir} -f cs-ab ${scene} ${YEARS}
-	${DIR}/dasgupta.py project -m ${model_dir} cs-ab ${scene} ${YEARS}
-
-	${DIR}/dasgupta.py combine ab ${scene} ${YEARS}
-	${DIR}/dasgupta.py combine cs-ab ${scene} ${YEARS}
-	${DIR}/dasgupta.py combine bii ${scene} ${YEARS}
-    done
-    ${DIR}/summarize.py --npp ${OUTDIR}/rcp/npp.tif
+	printf "%s %s\n" "${scene}" "${YEARS} ${model}"
+    done | xargs -P ${JOBS} -n 1 -L 1 ${DIR}/run.sh
+    ${DIR}/summarize.py --npp ${ODIR}/rcp/npp.tif
 
     mkdir -p ${model}-model
     mv Figure-1.png vivid-summary.csv ${model}-model
 
-    mkdir -p ${OUTDIR}/rcp/${model}-model
-    mv ${OUTDIR}/rcp/dasgupta-*-BIIAb-20*.tif ${OUTDIR}/rcp/${model}-model
-    mv ${OUTDIR}/rcp/dasgupta-*-Abundance-20*.tif ${OUTDIR}/rcp/${model}-model
-    mv ${OUTDIR}/rcp/dasgupta-*-CompSimAb-20*.tif ${OUTDIR}/rcp/${model}-model
+    mkdir -p ${ODIR}/rcp/${model}-model
+    mv ${ODIR}/rcp/dasgupta-*-BIIAb-20*.tif ${ODIR}/rcp/${model}-model
+    mv ${ODIR}/rcp/dasgupta-*-Abundance-20*.tif ${ODIR}/rcp/${model}-model
+    mv ${ODIR}/rcp/dasgupta-*-CompSimAb-20*.tif ${ODIR}/rcp/${model}-model
 done
---
+++ user-scripts/ricardog/dasgupta/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+scene="$1"
+years="$2"
+model="${3:-base}"
+if [[ -d /out ]]; then
+    model_dir=/out/models/dasgupta/2020-05-26/${model}
+else
+    model_dir=${HOME}/src/eec/predicts/models/dasgupta/2020-05-26/${model}
+fi
+
+echo ${scene}
+${DIR}/dasgupta.py project -m ${model_dir} -f ab ${scene} ${years}
+${DIR}/dasgupta.py project -m ${model_dir} ab ${scene} ${years}
+
+${DIR}/dasgupta.py project -m ${model_dir} -f cs-ab ${scene} ${years}
+${DIR}/dasgupta.py project -m ${model_dir} cs-ab ${scene} ${years}
+
+${DIR}/dasgupta.py combine ab ${scene} ${years}
+${DIR}/dasgupta.py combine cs-ab ${scene} ${years}
+${DIR}/dasgupta.py combine bii ${scene} ${years}

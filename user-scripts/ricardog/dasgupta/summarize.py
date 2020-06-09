bfd51f1c44122d478f366b4bc381ec5797418f8e
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-06-09 15:34:31 +0100

    Add support for summarizing other indicators
    The script was hard-wired to plot only BII.  This change makes it a
    little bit more flexible.

--- user-scripts/ricardog/dasgupta/summarize.py
+++ user-scripts/ricardog/dasgupta/summarize.py
@@ -12,23 +12,51 @@ import seaborn as sns
 
 import pdb
 
-def plot_by_scenario(data):
+def name_to_short(indicator):
+    if indicator == 'BIIAb':
+        return 'BII', 'bii'
+    if indicator == 'CompSimAb':
+        return 'CompSim', 'cs-ab'
+    if indicator == 'Abundance':
+        return 'Abundance', 'ab'
+    if indicator == 'Abundance-te':
+        return 'Temperate Abundance', 'ab-te'
+    if indicator == 'Abundance-tr':
+        return 'Tropical Abundance', 'ab-tr'
+    if indicator == 'Abundance-nf':
+        return 'Non-forest Abundance', 'ab-nf'
+    return None, None
+    
+def plot_by_scenario(data, indicator, npp):
+    t_ind, f_ind = name_to_short(indicator)
+    if npp is None:
+        npp_text = ' '
+    else:
+        npp_text = 'NPP-weighted '
+
     colors = ["windows blue", "amber", "faded green", "dusty purple"]
     palette = sns.xkcd_palette(colors)
     for scenario in data.Scenario.unique():
         subset = data.loc[data.Scenario == scenario]
-        sns.lineplot('Year', 'Mean', data=subset, hue='Mask',
+        sns.lineplot('Year', 'Mean', data=subset, hue='Mask', style='Mask',
                      linewidth=2, palette=palette)
         ax = plt.gca()
-        ax.set_ylabel('NPP-weighted Mean BII')
+        ax.set_ylabel(f'Mean {npp_text}{t_ind}')
         ax.set_xlabel('Year')
-        ax.set_title(f'Mean BII per model type ({scenario})')
-        plt.savefig(f'mean-bii-{scenario}.png')
+        ax.set_title(f'Mean {npp_text}{t_ind} per biome ({scenario})')
+        plt.savefig(f'mean-{f_ind}-{scenario}.png')
         plt.show()
+        plt.close()
     return
 
 
-def plot_all(data):
+def plot_all(data, indicator, npp):
+    t_ind, f_ind = name_to_short(indicator)
+    if npp is None:
+        npp_text = ' '
+    else:
+        npp_text = 'NPP-weighted '
+
     colors = ["windows blue", "amber", "dusty purple"]
     colors = ["windows blue", "amber", "faded green", "dusty purple",
               "scarlet"]
@@ -37,17 +65,22 @@ def plot_all(data):
     sns.lineplot('Year', 'Mean', data=data, hue='Scenario',
                  linewidth=2, palette=palette)
     ax = plt.gca()
-    ax.set_title('NPP-weighted Mean BII')
-    ax.set_ylabel('NPP-weighted Mean BII')
+    ax.set_title(f'Mean {npp_text}{t_ind}')
+    ax.set_ylabel(f'Mean {npp_text}{t_ind}')
     ax.set_xlabel('Year')
     plt.savefig('Figure-1.png')
     plt.show()
+    plt.close()
     return
 
 
 @click.command()
 @click.option('--npp', type=click.Path(dir_okay=False))
-def worldwide(npp):
+@click.option('--indicator', '-i', type=click.Choice(('BIIAb', 'Abundance',
+                                                      'CompSimAb',
+                                                      'Abundance-nf')),
+              default='BIIAb')
+def worldwide(npp, indicator):
     outdir = Path(os.environ.get('OUTDIR', '/out'), 'rcp')
     df = pd.DataFrame(columns=['Year', 'Scenario', 'Mask', 'Mean'])
     mask = {}
@@ -66,10 +99,13 @@ def worldwide(npp):
         with rasterio.open(Path(outdir, f'{layer}.tif')) as ds:
             mask[layer] = ds.read(1, masked=True) * land
             area[layer] = (mask[layer] * npp).sum()
-    for path in filter(lambda p: fnmatch(p.name, 'dasgupta-*-BIIAb-*.tif'),
+    #outdir = Path(os.environ.get('OUTDIR', '/out'), 'rcp', 'nohpd-model')
+    for path in filter(lambda p: fnmatch(p.name,
+                                         f'dasgupta-*-{indicator}-20*.tif'),
                        outdir.iterdir()):
         print(path)
-        _, scenario, what, year = path.stem.split('-')
+        _, scenario, _ = path.stem.split('-', 2)
+        _, year = path.stem.rsplit('-', 1)
         with rasterio.open(path) as src:
             data = src.read(1, masked=True) * npp
             df = df.append({'Year': int(year), 'Scenario': scenario,
@@ -86,10 +122,10 @@ def worldwide(npp):
     data = df.sort_values(['Year', 'Scenario']).reset_index(drop=True)
     #print(df.loc[df.Scenario == 'early'])
     #print(df.loc[df.Scenario == 'late'])
-    print(data.loc[df.Scenario == 'base'])
+    #print(data.loc[df.Scenario == 'base'])
     data.to_csv('vivid-summary.csv', index=False)
-    plot_by_scenario(data)
-    plot_all(data)
+    plot_by_scenario(data, indicator, npp)
+    plot_all(data, indicator, npp)
     return
 
 

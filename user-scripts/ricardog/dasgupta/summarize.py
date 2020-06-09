08db43b86ce787269586c7b21790e95b6e283e73
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-06-09 15:34:31 +0100

    Switch over to using seaborn for charts

--- user-scripts/ricardog/dasgupta/summarize.py
+++ user-scripts/ricardog/dasgupta/summarize.py
@@ -8,8 +8,40 @@ import os
 import pandas as pd
 from pathlib import Path
 import rasterio
+import seaborn as sns
 
 import pdb
+
+def plot_by_scenario(data):
+    colors = ["windows blue", "amber", "faded green", "dusty purple"]
+    palette = sns.xkcd_palette(colors)
+    for scenario in data.Scenario.unique():
+        subset = data.loc[data.Scenario == scenario]
+        sns.lineplot('Year', 'Mean', data=subset, hue='Mask',
+                     linewidth=2, palette=palette)
+        ax = plt.gca()
+        ax.set_ylabel('NPP-weighted Mean BII')
+        ax.set_xlabel('Year')
+        ax.set_title(f'Mean BII per model type ({scenario})')
+        plt.savefig(f'mean-bii-{scenario}.png')
+        plt.show()
+    return
+
+
+def plot_all(data):
+    colors = ["windows blue", "amber", "dusty purple"]
+    palette = sns.xkcd_palette(colors)
+    data = data.loc[data.Mask == 'Global']
+    sns.lineplot('Year', 'Mean', data=data, hue='Scenario',
+                 linewidth=2, palette=palette)
+    ax = plt.gca()
+    ax.set_title('NPP-weighted Mean BII')
+    ax.set_ylabel('NPP-weighted Mean BII')
+    ax.set_xlabel('Year')
+    plt.savefig('Figure-1.png')
+    plt.show()
+    return
+
 
 @click.command()
 @click.option('--npp', type=click.Path(dir_okay=False))
@@ -39,7 +71,7 @@ def worldwide(npp):
         with rasterio.open(path) as src:
             data = src.read(1, masked=True) * npp
             df = df.append({'Year': int(year), 'Scenario': scenario,
-                            'Mask': 'None',
+                            'Mask': 'Global',
                             'Mean': (data * land).sum() / land_area},
                            ignore_index=True)
             for mm in masks:
@@ -49,33 +81,13 @@ def worldwide(npp):
                                          area[mm])},
                                ignore_index=True)
                 
-    df = df.sort_values(['Year', 'Scenario']).reset_index(drop=True)
+    data = df.sort_values(['Year', 'Scenario']).reset_index(drop=True)
     #print(df.loc[df.Scenario == 'early'])
     #print(df.loc[df.Scenario == 'late'])
-    print(df.loc[df.Scenario == 'base'])
-    df.to_csv('vivid-sumary.csv', index=False)
-
-    for scenario in ('early', 'late', 'base'):
-        plt.plot(df.loc[(df.Scenario == scenario) & (df.Mask == 'tropical')].Year,
-                 df.loc[(df.Scenario == scenario) & (df.Mask == 'tropical')].Mean,
-                 label='Tropical')
-        plt.plot(df.loc[(df.Scenario == scenario) & (df.Mask == 'temperate')].Year,
-                 df.loc[(df.Scenario == scenario) & (df.Mask == 'temperate')].Mean,
-                 label='Temperate')
-        plt.plot(df.loc[(df.Scenario == scenario) & (df.Mask == 'nonforested')].Year,
-                 df.loc[(df.Scenario == scenario) & (df.Mask == 'nonforested')].Mean,
-                 label='Non-forested')
-        plt.plot(df.loc[(df.Scenario == scenario) & (df.Mask == 'None')].Year,
-                 df.loc[(df.Scenario == scenario) & (df.Mask == 'None')].Mean,
-                 label='Global', color='black', linewidth=2)
-        ax = plt.gca()
-        
-        ax.set_ylabel('NPP-weighted Mean BII')
-        ax.set_xlabel('Year')
-        ax.set_title(f'Mean BII per model type ({scenario})')
-        ax.legend()
-        plt.savefig(f'mean-bii-{scenario}.png')
-        plt.show()
+    print(data.loc[df.Scenario == 'base'])
+    data.to_csv('vivid-summary.csv', index=False)
+    #plot_by_scenario(data)
+    plot_all(data)
     return
 
 

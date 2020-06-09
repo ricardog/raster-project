28bb647e5368e91dcc403e25515b0d09952bdd96
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-06-09 15:34:31 +0100

    Adapt dasgupta/doit.sh to run all four models
    Adriana is now generating four model for the Dasgupta projections.  This
    commit makes doit.sh run all four models sequentially.  Data is stored
    in a per-model directory.

--- user-scripts/ricardog/dasgupta/doit.sh
+++ user-scripts/ricardog/dasgupta/doit.sh
@@ -1,26 +1,29 @@
 #!/bin/bash -e
 
-#MODEL_DIR=$HOME/src/eec/predicts/models/dasgupta/2020-05-22
-#MODEL_DIR=$HOME/src/eec/predicts/models/dasgupta/2020-05-22/nohpd
-#MODEL_DIR=$HOME/src/eec/predicts/models/dasgupta/2020-05-22/nohpd-ageclass
-MODEL_DIR=$HOME/src/eec/predicts/models/dasgupta/2020-05-22/ageclass
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 YEARS=2020:2061:5
-#for scene in early late base; do
-for scene in base early late_23 late_26 late_29 ; do
-    ${DIR}/dasgupta.py project -m ${MODEL_DIR} -f ab ${scene} ${YEARS}
-    ${DIR}/dasgupta.py project -m ${MODEL_DIR} ab ${scene} ${YEARS}
+for model in base ageclass nohpd nohpd-ageclass; do
+    echo ${model}
+    model_dir=$HOME/src/eec/predicts/models/dasgupta/2020-05-26/${model}
+    for scene in base early late_23 late_26 late_29 ; do
+	echo ${scene}
+	${DIR}/dasgupta.py project -m ${model_dir} -f ab ${scene} ${YEARS}
+	${DIR}/dasgupta.py project -m ${model_dir} ab ${scene} ${YEARS}
 
-    ${DIR}/dasgupta.py project -m ${MODEL_DIR} -f cs-ab ${scene} ${YEARS}
-    ${DIR}/dasgupta.py project -m ${MODEL_DIR} cs-ab ${scene} ${YEARS}
+	${DIR}/dasgupta.py project -m ${model_dir} -f cs-ab ${scene} ${YEARS}
+	${DIR}/dasgupta.py project -m ${model_dir} cs-ab ${scene} ${YEARS}
 
-    ${DIR}/dasgupta.py combine ab ${scene} ${YEARS}
-    ${DIR}/dasgupta.py combine cs-ab ${scene} ${YEARS}
-    ${DIR}/dasgupta.py combine bii ${scene} ${YEARS}
+	${DIR}/dasgupta.py combine ab ${scene} ${YEARS}
+	${DIR}/dasgupta.py combine cs-ab ${scene} ${YEARS}
+	${DIR}/dasgupta.py combine bii ${scene} ${YEARS}
+    done
+    ${DIR}/summarize.py --npp ${OUTDIR}/rcp/npp.tif
 
-    #${DIR}/dasgupta.py mask -m ${MODEL_DIR} -f ab ${scene} ${YEARS}
-    #${DIR}/dasgupta.py mask -m ${MODEL_DIR} ab ${scene} ${YEARS}
+    mkdir -p ${model}-model
+    mv Figure-1.png vivid-summary.csv ${model}-model
 
-    #${DIR}/dasgupta.py mask -m ${MODEL_DIR} -f cs-ab ${scene} ${YEARS}
-    #${DIR}/dasgupta.py mask -m ${MODEL_DIR} cs-ab ${scene} ${YEARS}
+    mkdir -p ${OUTDIR}/rcp/${model}-model
+    mv ${OUTDIR}/rcp/dasgupta-*-BIIAb-20*.tif ${OUTDIR}/rcp/${model}-model
+    mv ${OUTDIR}/rcp/dasgupta-*-Abundance-20*.tif ${OUTDIR}/rcp/${model}-model
+    mv ${OUTDIR}/rcp/dasgupta-*-CompSimAb-20*.tif ${OUTDIR}/rcp/${model}-model
 done

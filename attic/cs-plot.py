4c455dd6e21a151d50cd0739dd78069888a8805e
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-06-09 15:34:31 +0100

    Converted script to plot response curves for Dasgupta models
    The script is a bit more generic now in that it can take a prefix to
    search for in the model variables and generate curves for them.
    
    To run it use something like
    
    ./attic/cs-plot.py -p magpie_baseline_pas_contrast_primary_minimal_ cs_nf.rds

--- attic/cs-plot.py
+++ attic/cs-plot.py
@@ -5,6 +5,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from pprint import pprint
 import re
 import seaborn as sns
 
@@ -14,64 +15,85 @@ import projections.lu.luh2 as luh2
 import pdb
 
 def inv_logit(p):
-  """Returns the inverse logit function of the input."""
-  return np.exp(p) / (1 + np.exp(p))
+    """Returns the inverse logit function of the input."""
+    return np.exp(p) / (1 + np.exp(p))
 
 @click.command()
 @click.argument('model', type=click.Path(dir_okay=False))
-@click.option('--title', '-t', type=str,
+@click.option('--title', type=str,
               default='Model response vs HPD',
               help='Plot title')
 @click.option('--adjust', type=float,
-              default=0.1,
+              default=0.01,
               help='Adjustment value for inverse logit transformation')
-@click.option('--save', type=click.Path(dir_okay=False),
+@click.option('--save', '-s', type=click.Path(dir_okay=False),
               help='A file to save the plot to.')
+@click.option('--prefix', '-p', type=str)
 @click.option('--tropical', '-t', is_flag=True, default=False)
-def plot(model, title, save, adjust, tropical):
-  """Plot response curve of CS model versus HPD."""
-  mod = modelr.load(model)
+def plot(model, title, save, adjust, prefix, tropical):
+    """Plot response curve of CS model versus HPD."""
+    colors = ["windows blue", "amber", "faded green", "dusty purple",
+              "scarlet", "eggplant"]
+    palette = sns.xkcd_palette(colors)
+    #palette = sns.color_palette('colorblind', 6)
+    #flatui = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+    #palette = sns.color_palette(flatui)
 
-  columns = tuple(filter(lambda x: re.search(r'_age\d+$', x), mod.syms))
-  df = pd.DataFrame(columns=tuple(luh2.LU.keys()))
-  df = pd.DataFrame(columns=columns)
-  pname1 = 'forested_tropic_temperate_tropical_forest'
-  pname2 = 'tropic_temperate_tropical_forest_tropical_forest'
+    cb = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33']
+    palette = sns.color_palette(cb)
 
-  tropical = 1 if tropical else 0
-    
-  for col in df.columns:
-    s = pd.Series(mod.partial({'loghpd': np.linspace(0, 11, 13),
-                               's2_loghpd': np.linspace(0, 11, 13),
-                               'hpd_diff': np.linspace(0, -11, 13),
-                               pname1: tropical,
-                               pname2: tropical,
-                               col: np.full((13), 1)}))
+    mod = modelr.load(model)
+
+    columns = tuple(filter(lambda x: re.match(prefix, x), mod.syms))
+    #df = pd.DataFrame(columns=tuple(luh2.LU.keys()))
+    df = pd.DataFrame(columns=columns)
+    pname1 = 'forested_tropic_temperate_tropical_forest'
+    pname2 = 'tropic_temperate_tropical_forest_tropical_forest'
+
+    tropical = 1 if tropical else 0
     intercept = mod.intercept
 
-    if mod.output == 'sqrtRescaledAbundance':
-        df[col] = np.power(s, 2) / np.power(intercept, 2)
+    for col in df.columns:
+        s = pd.Series(mod.partial({'loghpd': np.linspace(0, 11, 13),
+                                   's2_loghpd': np.linspace(0, 11, 13),
+                                   'hpd_diff': np.linspace(0, -11, 13),
+                                   pname1: tropical,
+                                   pname2: tropical,
+                                   col: np.full((13), 1)}))
+        s = s.clip(*mod.output_range)
+        if mod.output == 'sqrtRescaledAbundance':
+            vname = 'Abundance'
+            df[col] = np.power(s, 2) / np.power(intercept, 2)
+        else:
+            vname = 'CompSim'
+            df[col] = (inv_logit(s) - adjust) / (inv_logit(intercept) - adjust)
+
+    #pdb.set_trace()
+    df.index = np.linspace(0, 11, 13)
+    df = df.rename(columns=lambda c: c.replace(prefix, ''))
+
+    if prefix[-3:] == 'age':
+        df = df.rename(columns=lambda c: 'age_' + c)
+        df2 = pd.wide_to_long(df.assign(loghpd=df.index), ['age'], i='loghpd',
+                              j='Age', sep='_').reset_index()\
+                .rename(columns=lambda x: x if x != 'age' else vname)
+        df2.Age = df2.Age.astype('category')
+        sns.lineplot('loghpd', vname, hue='Age', data=df2,
+                     linewidth=1.5, palette=palette)
     else:
-        df[col] = (inv_logit(s) - adjust) / (inv_logit(intercept) - adjust)
+        df.plot()
+    ax = plt.gca()
+    ax.set_title(title)
+    ax.set_xlabel('log(HPD + 1)')
+    ax.set_ylabel(vname)
 
-  prefix = df.columns[0].rsplit('_', 1)[0]
-  df = df.rename(columns=lambda c: c.replace(prefix + '_', ''))
-  df.plot()
-  ax = plt.gca()
-  ax.set_title(title)
-  ax.set_xlabel('log(HPD + 1)')
-  if mod.output == 'sqrtRescaledAbundance':
-    ax.set_ylabel('Abundance')
-  else:
-    ax.set_ylabel('CompSim')
+    if save:
+        plt.savefig(save, transparent=False, bbox_inches="tight", pad_inches=0)
+    plt.show()
+    return
 
-  if save:
-    plt.savefig(save, transparent=True, bbox_inches="tight", pad_inches=0)
-  plt.show()
 
 if __name__ == '__main__':
-  matplotlib.style.use('ggplot')
-  sns.set_style("darkgrid")
-#pylint: disable-msg=no-value-for-parameter
-  plot()
-#pylint: enable-msg=no-value-for-parameter
+    matplotlib.style.use('ggplot')
+    sns.set_style("darkgrid")
+    plot()

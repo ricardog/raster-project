53f8f0e3da73623c339d0a6965adad8b809c7fd6
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-06-09 15:34:31 +0100

    Script to convert Dasgupta BII rasters into a short video
    There are very few frames so it doesn't make sense to watch as a video
    but one can usefully scrub back-and-forth to see the changes per 5-year
    period.

---
+++ user-scripts/ricardog/dasgupta/to-mp4.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import click
+from fnmatch import fnmatch
+import matplotlib.colors as colors
+
+import os
+from pathlib import Path
+import rasterio
+from projections.mp4_utils import to_mp4
+
+def parse_fname2(fname):
+  return os.path.splitext(os.path.basename(fname))[0].rsplit('-', 3)
+
+
+@click.command()
+@click.argument('pattern', type=str)
+@click.argument('oname', type=click.Path(dir_okay=False))
+@click.option('--title', '-t', type=str)
+@click.option('--fps', type=int, default=10)
+def convert(pattern, oname, title, fps):
+    outdir = Path(os.environ.get('OUTDIR', '/out'), 'rcp')
+    files = [path for path in outdir.iterdir() if fnmatch(path.name, pattern)]
+    files = sorted(files)
+    nframes = len(files)
+    cnorm = colors.Normalize(vmin=0.0, vmax=1.2)
+    with rasterio.open(files[0]) as src:
+        data = src.read(1, masked=True)
+    if not title:
+        title = pattern
+    for idx, img, text in to_mp4(title, oname, nframes,
+                                 data, 'year', fps, cnorm=cnorm):
+        _, scenario, metric, year = parse_fname2(files[idx])
+        with rasterio.open(files[idx]) as ds:
+            data = ds.read(1, masked=True)
+        img.set_array(data)
+        text.set_text(year)
+    return
+
+
+if __name__ == '__main__':
+    convert()

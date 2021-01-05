0425a0404ddcb7f190cdaf4de5a9c89d04887885
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-01-05 17:58:57 +0000

    Untabify
    There was a tab in one of the console_script lines which caused the
    parsing to go wonky.
    
    I also added a number of missing fields and a couple of missing packages
    that are now required.

--- setup.py
+++ setup.py
@@ -2,36 +2,44 @@ from setuptools import setup, find_packages
 
 setup(
     name='projections',
+    author="Ricardo E. Gonzalez",
+    author_email="ricardog@ricardog.com",
+    description="Spatio-temporal projections of R models using python",
     version='0.1',
+    python_requires='>=3.6.3',
+    url="https://github.com/NaturalHistoryMuseum/raster-project",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-      'Click',
-      'gdal',
-      'fiona',
-      'geopy',
-      'joblib',
-      'matplotlib',
-      'netCDF4',
-      'numba',
-      'numpy',
-      'pandas',
-      'pylru',
-      'pyparsing',
-      'rasterio',
-      'rpy2',
-      'setuptools',
-      'shapely',
-      'tqdm',
-      'xlrd',
+        'Click',
+        'asciitree',
+        'gdal',
+        'fiona',
+        'geopy',
+        'inflection',
+        'joblib',
+        'matplotlib',
+        'netCDF4',
+        'numba',
+        'numpy',
+        'pandas',
+        'pylru',
+        'pyparsing',
+        'rasterio',
+        'rpy2',
+        'setuptools',
+        'shapely',
+        'tqdm',
+        'xlrd',
     ],
     entry_points='''
         [console_scripts]
         extract_values=projections.scripts.extract_values:main
         gen_hyde=projections.scripts.gen_hyde:main
         gen_sps=projections.scripts.gen_sps:main
-	hyde2nc=projections.scripts.hyde2nc:main
+        hyde2nc=projections.scripts.hyde2nc:main
         nc_dump=projections.scripts.nc_dump:main
+
         nctomp4=projections.scripts.nctomp4:main
         project=projections.scripts.project:cli
         r2py=projections.scripts.r2py:main

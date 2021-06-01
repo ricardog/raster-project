114297f97121fecdeca3d5042e81f4c6e029b9c1
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-06-01 10:38:16 +0100

    Update setup.py

--- projections/setup.py
+++ projections/setup.py
@@ -5,54 +5,35 @@ setup(
     version="0.1",
     author="Ricardo E. Gonzalez",
     author_email="ricardog@itinerisinc.com",
-    description="Spatio-temporal projections of R models using python",
+    description="Spatio-temporal projections of PREDICTS models",
     python_requires=">=3.8.3",
     url="https://github.com/NaturalHistoryMuseum/raster-project",
     project_urls={
-        "Bug Tracker": "https://github.com/ricardog/raster-project/issues",
+        "Bug Tracker": "https://github.com/NaturalHistoryMuseum/raster-project/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
     install_requires=[
-        "cartopy",
         "Click",
-        "gdal",
         "fiona",
         "geopandas",
-        "geopy",
         "joblib",
         "matplotlib",
         "netCDF4",
         "numpy",
         "pandas",
+        "projutils @ git+https://github.com/ricardog/projutils",
         "pylru",
         "r2py @ git+https://github.com/ricardog/r2py",
         "rasterset @ git+https://github.com/ricardog/rasterset",
         "rasterio",
-        "scipy",
         "setuptools",
         "shapely",
-        "tqdm",
-        "xlrd",
     ],
-    entry_points="""
-        [console_scripts]
-        extract_values=projections.scripts.extract_values:main
-        gen_hyde=projections.scripts.gen_hyde:main
-        gen_sps=projections.scripts.gen_sps:main
-        hyde2nc=projections.scripts.hyde2nc:main
-        nc_dump=projections.scripts.nc_dump:main
-
-        nctomp4=projections.scripts.nctomp4:main
-        project=projections.scripts.project:cli
-        rview=projections.scripts.rview:main
-        tifftomp4=projections.scripts.tifftomp4:main
-        tiffcmp=projections.scripts.tiffcmp:main
-    """,
 )

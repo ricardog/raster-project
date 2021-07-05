29b7c7234bd192410568e466b18b09709b989e4b
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-07-05 18:08:39 +0100

    Update model directory
    Use the latest model directory.

--- user-scripts/ricardog/restore/run.sh
+++ user-scripts/ricardog/restore/run.sh
@@ -4,11 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 scene="$1"
 years="$2"
 model="${3:-base}"
-if [[ -d /out ]]; then
-    model_dir=/out/models//2020-06-02/${model}
-else
-    model_dir=${HOME}/src/eec/predicts/models/brazil/2020-10-27/${model}
-fi
+model_dir=/mnt/predicts/models/brazil/2021-07-01/${model}
 
 echo ${scene}
 ${DIR}/restore.py project -m ${model_dir} ab ${scene} ${years}

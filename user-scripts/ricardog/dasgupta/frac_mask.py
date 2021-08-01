2d59388e59221de8e948ee92a800548178253198
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-08-01 13:39:59 +0100

    Enhance fractional mask script to deal with rasters not a multiple of factor
    If the source raster is not a multiple of factor, we need to read a few
    extra lines so we can then decimate easily.

--- user-scripts/ricardog/dasgupta/frac_mask.py
+++ user-scripts/ricardog/dasgupta/frac_mask.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
+import math
+
 import click
 import rasterio
+from rasterio.coords import BoundingBox
+
+
+def round(left, bottom, top, right, res):
+    def round_down(x, a):
+        return math.floor(x / a) * a
+
+    def round_up(x, a):
+        return math.ceil(x / a) * a
+
+    def round_to_inf(x, a):
+        if a * x < 0:
+            return round_down(x, a)
+        return round_up(x, a)
+    return BoundingBox(round_to_inf(left, res[0]),
+                       round_to_inf(bottom, res[1]),
+                       round_to_inf(top, res[0]),
+                       round_to_inf(right, res[1])
+                       )
 
 
 @click.command()
@@ -15,8 +36,13 @@ def main(factor, in_file, out_file):
         meta["height"] //= factor
         meta["transform"] *= src.transform.scale(factor)
         meta["nodata"] = -9999.0
-        data = src.read(1, masked=True)
-        out = data.reshape(meta["height"], factor, meta["width"], factor).sum(3).sum(1)
+        bounds = round(*src.bounds, (meta['transform'].a * factor,
+                                     meta['transform'].e * factor))
+        win = src.window(*bounds)
+        width = int(win.width // factor)
+        height = int(win.height // factor)
+        data = src.read(1, masked=True, window=win, boundless=True)
+        out = data.reshape(height, factor, width, factor).sum(3).sum(1)
         out /= factor ** 2
         out.fill_value = -9999.0
         with rasterio.open(out_file, "w", **meta) as dst:
